8d62051b27c636a023efe501efee96eb35413b36
Rosa Kang <therosessom@gmail.com>
2022-07-14 10:05:42 -0700

    update minor

--- 17-database.py
+++ 17-database.py
@@ -1,3 +1,5 @@
 # Database Design
 # Never put the same string of data more than once.**
 # What is the best practice for how many times a piece of string data should be stored in a database? 1
+# What is a foreign key?
+# A number that points to the primary key of an associated row in a different table.
