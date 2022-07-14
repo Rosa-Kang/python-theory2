2c1037a5cb9044c54e019476516346c5c7fcbbcf
Rosa Kang <therosessom@gmail.com>
2022-07-14 10:30:02 -0700

    data-sql

--- 17-database.py
+++ 17-database.py
@@ -3,3 +3,11 @@
 # What is the best practice for how many times a piece of string data should be stored in a database? 1
 # What is a foreign key?
 # A number that points to the primary key of an associated row in a different table.
+# Three Kinds of Keys.
+# 1. Primary key - generally an integer auto-increment field
+# 2. Logical key - what the outside world uses for lookup
+# 3. Foreign key - generally an integer key pointing to a row in another table.
+
+
+# What does the INSERT command do in SQL?
+# It defines a new row by listing the fields we want to include followed by the values we want to placed in the new row.
