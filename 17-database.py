4c2a55a8b59c7590a9afcca687b76002a32c154d
Rosa Kang <therosessom@gmail.com>
2022-07-14 19:23:26 -0700

    2022-07-14

--- 17-database.py
+++ 17-database.py
@@ -9,5 +9,53 @@
 # 3. Foreign key - generally an integer key pointing to a row in another table.
 
 
-# What does the INSERT command do in SQL?
+# What does the INSERT command do in SQL?   
 # It defines a new row by listing the fields we want to include followed by the values we want to placed in the new row.
+
+
+#Many to Many relationships
+from inspect import CORO_SUSPENDED
+
+
+CREATE  TABLE  User (
+id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    name  TEXT UNIQUE,
+    email  TEXT
+);
+    
+
+CREATE  TABLE  Course (
+id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    title  TEXT UNIQUE
+);
+    
+
+CREATE  TABLE  Member (
+user_id  INTEGER,
+    course_id  INTEGER,
+    role  INTEGER,
+    PRIMARY KEY (user_id, course_id)
+);
+    
+INSERT INTO User (name, email) VALUES ('Jane', 'jane@gmail.com');
+INSERT INTO User (name, email) VALUES ('Tonny', 'tonny@gmail.com');
+INSERT INTO User (name, email) VALUES ('Kate', 'kate@gmail.com');
+
+INSERT INTO Course (title) VALUES ('Python');
+INSERT INTO Course (title) VALUES ('SQL');
+INSERT INTO Course (title) VALUES ('PHP');
+
+INSERT INTO Member (user_id, course_id, role) VALUES (1, 1, 1);
+INSERT INTO Member (user_id, course_id, role) VALUES (2, 1, 0);
+INSERT INTO Member (user_id, course_id, role) VALUES (3, 1, 0);
+
+INSERT INTO Member (user_id, course_id, role) VALUES (1, 2, 0);
+INSERT INTO Member (user_id, course_id, role) VALUES (2, 2, 1);
+INSERT INTO Member (user_id, course_id, role) VALUES (2, 3, 1);
+INSERT INTO Member (user_id, course_id, role) VALUES (3, 3, 0);
+
+SELECT User.name, Member.role, Course.title
+FROM User JOIN Member JOIN Course
+ON Member.user_id = User.id AND
+Member.course_id = Course.id
+ORDER BY Course.title, Member.role DESC, User.name
