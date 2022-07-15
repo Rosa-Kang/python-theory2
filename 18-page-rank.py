94d8506830c61bed3c1dde0d589f8a4af5f4d48b
Rosa Kang <therosessom@gmail.com>
2022-07-14 20:06:10 -0700

    update

---
+++ 18-page-rank.py
@@ -0,0 +1,8 @@
+# Search Engine Architecture
+# Web Crawling
+# Index Building - search index
+# Searching
+
+# How does the PageRank algorithm work? : It determines which pages are most highly connected.
+
+# Mailing Lists
